9dd94dc0a19da4338d35c1a52889fc2c913f3676
antecume547 <79765534+antecume547@users.noreply.github.com>
2021-09-25 21:34:10 +0200

    linting final

--- final_project/machinetranslation/translator.py
+++ final_project/machinetranslation/translator.py
@@ -1,12 +1,12 @@
 """
-Module for initializing IBM Watson Language Translator 
+Module for initializing IBM Watson Language Translator
 """
 import os
 from ibm_watson import LanguageTranslatorV3
 from ibm_cloud_sdk_core.authenticators import IAMAuthenticator
 from dotenv import load_dotenv
 
-#import these for debugging purpose 
+#import these for debugging purpose
 #import sys, traceback
 #import json
 #from ibm_watson import ApiException
